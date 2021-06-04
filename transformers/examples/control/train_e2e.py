67c2a9a18a28509d116d6c8dee0b2ed3ec33fce5
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2021-06-04 13:51:47 -0700

    Refactor.

--- transformers/examples/control/train_e2e.py
+++ transformers/examples/control/train_e2e.py
@@ -1,26 +1,31 @@
-import os, sys
 import argparse
+import os
+import sys
 
 # example: python train_run.py keyword temp_keyword _
 if not sys.warnoptions:
     import warnings
+
     warnings.simplefilter("ignore")
 
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser(description='data2text E2E training args.')
-    parser.add_argument('--mode', type=str, default='data2text', help='')
-    parser.add_argument('--tuning_mode', type=str, default='prefixtune', help='')
-    parser.add_argument('--optim_prefix', type=str, default='yes', help='')
+    parser.add_argument('--mode', type=str, default='data2text',
+                        choices=(
+                            'data2text', 'triples', 'webnlg', 'writingPrompts', 'cnndm', 'xsum', 'sentiment', 'topic',
+                            'classify-sentiment', 'classify-topic')
+                        )
+    parser.add_argument('--tuning_mode', type=str, default='prefixtune',
+                        choices=('prefixtune', 'finetune', 'finetune-top', 'bothtune', 'adaptertune'))
+    parser.add_argument('--optim_prefix', type=str, default='yes', choices=('yes', 'no'))
     parser.add_argument('--preseqlen', type=int, default=10, help='')
-    parser.add_argument('--prefix_mode', type=str, default='activation', help='')
-    parser.add_argument('--format_mode', type=str, default='cat', help='')
+    parser.add_argument('--prefix_mode', type=str, default='activation', choices=('embedding', 'activation'))
+    parser.add_argument('--format_mode', type=str, default='cat', choices=('cat', 'infix', 'peek', 'nopeek'))
 
     parser.add_argument('--dir_name', type=str, default=None, help='')
     parser.add_argument('--notes', type=str, default=None, help='')
     parser.add_argument('--lowdata_token', type=str, default='summarize', help='')
     parser.add_argument('--use_lowdata_token', type=str, default='yes', help='')
-
 
     parser.add_argument('--parametrize_emb', type=str, default='MLP', help='')
     parser.add_argument('--adapter_design', type=int, default=1, help='')
@@ -28,10 +33,9 @@ if __name__ == '__main__':
 
     parser.add_argument('--objective_mode', type=int, default=1, help='')
 
-
     # training parameters.
     parser.add_argument('--use_dropout', type=str, default='no', help='')
-    parser.add_argument('--seed', type=int, default=101, help='') # old is 42
+    parser.add_argument('--seed', type=int, default=101, help='')  # old is 42
     parser.add_argument('--bsz', type=int, default=10, help='')
     parser.add_argument('--use_big', type=str, default='no', help='')
     parser.add_argument('--epoch', type=int, default=5, help='')
@@ -50,19 +54,12 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    assert args.optim_prefix in ['yes', 'no']
     if args.optim_prefix == 'yes':
         assert args.preseqlen is not None
-    assert args.prefix_mode in ['embedding', 'activation']
-    assert args.format_mode in ['cat', 'infix', 'peek', 'nopeek']
-    assert args.tuning_mode in ['prefixtune', 'finetune', 'finetune-top', 'bothtune', 'adaptertune']
     if args.prefix_model_path is not None:
         load_prefix_model = True
     else:
         load_prefix_model = False
-
-    assert  args.mode in ['data2text', 'triples', 'webnlg', 'writingPrompts', 'cnndm', 'xsum', 'sentiment', 'topic',
-                          'classify-sentiment', 'classify-topic']
 
     assert args.objective_mode in [0, 1]
     # 0 means the regular token level objective, which is sum / output_len
@@ -70,7 +67,6 @@ if __name__ == '__main__':
     # 2 means our buggy version which is sum/max_batch(input_len +output_len)
     # 3 means our buggy version which is sum/max_batch(output_len)
     # 4 means our buggy version which is sum/(input_len +output_len)
-
 
     if args.tuning_mode == 'adaptertune':
         folder_name = 'baseline_adapter/'
@@ -85,17 +81,13 @@ if __name__ == '__main__':
         else:
             args.notes = args.notes + '_l={}'.format(args.top_layers)
 
-
     if args.mode == 'data2text':
-
         TRAIN_FILE = "/u/scr/xlisali/e2e_data/src1_train.txt"
         TEST_FILE = "/u/scr/xlisali/e2e_data/src1_valid.txt"
-        # folder_name = 'save_e2e_models_infix/'
         folder_name = 'save_e2e_models_convcheck/'
 
         if args.prefix_mode == 'embedding':
             folder_name = 'ablation_e2e_emb_models/'
-
             if args.notes is None:
                 args.notes = args.parametrize_emb
             else:
@@ -104,15 +96,13 @@ if __name__ == '__main__':
         if args.format_mode == 'infix':
             folder_name = 'ablation_e2e_infix_models/'
 
-
-
         if args.notes is not None and 'lowdata' in args.notes:
             _, temp_seed, temp_size = args.notes.split('_')
             TRAIN_FILE = "/juice/u/xlisali/e2e_lowdata/lowdata_{}_{}_train.txt".format(temp_seed, temp_size)
             TEST_FILE = "/juice/u/xlisali/e2e_lowdata/lowdata_{}_{}_valid.txt".format(temp_seed, temp_size)
             # folder_name = 'e2e_lowdata_models_new/' #100
             # folder_name = 'e2e_lowdata_models_finetune/'
-            folder_name = 'e2e_lowdata_models_prefixtune/' # 50, 200
+            folder_name = 'e2e_lowdata_models_prefixtune/'  # 50, 200
 
             if temp_size == '10':
                 pass
@@ -127,7 +117,6 @@ if __name__ == '__main__':
                 # args.max_steps = 400
                 # args.eval_steps = 50
 
-
             app_special = ' --max_steps {} --eval_steps {} --save_steps -1 ' \
                           '--evaluate_during_training --per_device_eval_batch_size 32 ' \
                           '--warmup_steps {} --lowdata_token {} ' \
@@ -137,7 +126,6 @@ if __name__ == '__main__':
 
             args.notes = args.notes + 'st={}_ev={}_ws={}_t={}'.format(args.max_steps, args.eval_steps,
                                                                       args.warmup_steps, args.lowdata_token)
-
 
         if args.notes in ['Type', 'near', 'customer', 'food', 'area', 'price']:
             TRAIN_FILE = "/u/scr/xlisali/e2e_data/missing_{}_cleaned_train_e2e.txt".format(args.notes[:4])
@@ -189,8 +177,9 @@ if __name__ == '__main__':
         test_max_target_length = 142
 
         cnndm_app = ' --max_source_length {} --train_max_target_length {} ' \
-                   '--val_max_target_length {} --dataloader_num_workers 4 '.format(max_source_length, max_target_length,
-                                                         val_max_target_length, )
+                    '--val_max_target_length {} --dataloader_num_workers 4 '.format(max_source_length,
+                                                                                    max_target_length,
+                                                                                    val_max_target_length, )
 
         folder_name = "cnndm_models/"
         assert args.optim_prefix == 'yes'
@@ -206,7 +195,7 @@ if __name__ == '__main__':
 
         xsum_app = ' --max_source_length {} --train_max_target_length {} ' \
                    '--val_max_target_length {} --dataloader_num_workers 4 '.format(max_source_length, max_target_length,
-                                                         val_max_target_length, )
+                                                                                   val_max_target_length, )
 
         folder_name = "xsum_models/"
         assert args.optim_prefix == 'yes'
@@ -233,9 +222,6 @@ if __name__ == '__main__':
         folder_name = "classification_models/"
         assert args.optim_prefix == 'yes'
 
-
-
-
     batch_size = args.gradient_accumulation_steps * args.bsz
     # print(args.mode + args.tuning_mode + '_' + args.optim_prefix[:1] + '_' + args.preseqlen)
     # print('_' + args.prefix_mode[:2] + '_' + args.format_mode[:2] + '_')
@@ -250,7 +236,8 @@ if __name__ == '__main__':
                      '_' + args.prefix_mode[:3] + '_' + args.format_mode[:3] + '_' + \
                      'b={}-'.format(batch_size) + 'e={}_'.format(args.epoch) + 'd={}_'.format(args.dropout) + \
                      'u={}_'.format(args.use_dropout) + 'lr={}_'.format(args.learning_rate) \
-                     + 'w={}_'.format(args.weight_decay) + 's={}'.format(args.seed) + '_r={}'.format(args.init_random[:1]) +\
+                     + 'w={}_'.format(args.weight_decay) + 's={}'.format(args.seed) + '_r={}'.format(
+            args.init_random[:1]) + \
                      '_m={}'.format(args.mid_dim)
     else:
         Model_FILE = dir_name
@@ -264,7 +251,6 @@ if __name__ == '__main__':
     Model_FILE = '{}{}'.format(folder_name, Model_FILE)
     print(Model_FILE)
 
-
     if args.notes is not None and 'large' in args.notes:
         OLD_MODEL = "gpt2-large"
     else:
@@ -272,7 +258,7 @@ if __name__ == '__main__':
 
     app = "--optim_prefix {} --preseqlen {} --prefix_mode {} --format_mode {} " \
           "--gradient_accumulation_steps {} --learning_rate {} --weight_decay {} --seed {} --disable_tqdm " \
-          "--mid_dim {} --init_random {} --use_dropout {} --prefix_dropout {} --objective_mode {} ".\
+          "--mid_dim {} --init_random {} --use_dropout {} --prefix_dropout {} --objective_mode {} ". \
         format(args.optim_prefix, args.preseqlen, args.prefix_mode, args.format_mode,
                args.gradient_accumulation_steps, args.learning_rate, args.weight_decay, args.seed,
                args.mid_dim, args.init_random, args.use_dropout, args.dropout, args.objective_mode)
@@ -295,14 +281,11 @@ if __name__ == '__main__':
     if args.tuning_mode == 'finetune-top':
         app += ' --top_layers {} '.format(args.top_layers)
 
-
     if args.mode == 'xsum':
         app += xsum_app
 
     if args.mode == 'cnndm':
         app += cnndm_app
-
-
 
     # when we have input-based AND activation level AND infix.
     # app = "--optim_prefix {} --preseqlen {} --prefix_mode {} --format_mode {} " \
@@ -320,11 +303,9 @@ if __name__ == '__main__':
     # We have instruction based AND activation AND peek.
     # app = "--optim_prefix {} --preseqlen {} --prefix_mode {} --format_mode {}".format('yes', 15, 'activation', 'peek')
 
-
-
     controlprefix = ('yes' if args.tuning_mode == 'prefixtune' else 'no')
 
-    COMMANDLINE="python run_language_modeling.py \
+    COMMANDLINE = "python run_language_modeling.py \
         --output_dir={} \
         --model_type=gpt2 \
         --model_name_or_path={} \
@@ -342,7 +323,8 @@ if __name__ == '__main__':
         --task_mode {} \
         --eval_data_file={}  \
         --dataless no --tuning_mode {} --logging_dir {} \
-        --train_embs no ".format(Model_FILE, OLD_MODEL, OLD_MODEL, args.bsz, args.bsz, args.epoch, TRAIN_FILE, args.mode, TEST_FILE,
+        --train_embs no ".format(Model_FILE, OLD_MODEL, OLD_MODEL, args.bsz, args.bsz, args.epoch, TRAIN_FILE,
+                                 args.mode, TEST_FILE,
                                  args.tuning_mode, logging_dir)
 
     COMMANDLINE += app
@@ -351,46 +333,41 @@ if __name__ == '__main__':
         LOAD_TRAIN_PREFIX = '/u/scr/xlisali/contrast_LM/transformers/examples/control/med_topic_gen'
         COMMANDLINE += '--prefixModel_name_or_path {} '.format(LOAD_TRAIN_PREFIX)
 
-
-
     with open(Model_FILE + '.sh', 'w') as f:
         print(COMMANDLINE, file=f)
 
-
     print(COMMANDLINE)
     if args.submit == 'no':
-        os.system(COMMANDLINE) # textattack/roberta-base-ag-news # textattack/roberta-base-imdb
+        os.system(COMMANDLINE)  # textattack/roberta-base-ag-news # textattack/roberta-base-imdb
     # #
     elif args.submit == 'yes':
         if args.use_big == 'no':
-            full_command = "nlprun -a lisa-base-torch -g 1 -n {} -x jagupard4,jagupard5,jagupard6,jagupard7,jagupard8,jagupard14 \'{}\'".format(Model_FILE, COMMANDLINE)
+            full_command = "nlprun -a lisa-base-torch -g 1 -n {} -x jagupard4,jagupard5,jagupard6,jagupard7," \
+                           "jagupard8,jagupard14 \'{}\'".format(
+                Model_FILE, COMMANDLINE)
             if args.mode == 'cnndm':
-                full_command ="nlprun -a lisa-base-torch -r 20GB -g 1 -n {} -x jagupard4,jagupard5,jagupard6,jagupard7,jagupard8 \'{}\'".format(Model_FILE, COMMANDLINE)
+                full_command = "nlprun -a lisa-base-torch -r 20GB -g 1 -n {} -x jagupard4,jagupard5,jagupard6," \
+                               "jagupard7,jagupard8 \'{}\'".format(
+                    Model_FILE, COMMANDLINE)
         elif True:
-            full_command = "nlprun -p high -a lisa-base-torch -g 1 -n {} -x jagupard4,jagupard5,jagupard6,jagupard7,jagupard8," \
-                           "jagupard10,jagupard11,jagupard12,jagupard13,jagupard14,jagupard15,jagupard16,jagupard17,jagupard18," \
+            full_command = "nlprun -p high -a lisa-base-torch -g 1 -n {} -x jagupard4,jagupard5,jagupard6,jagupard7," \
+                           "jagupard8," \
+                           "jagupard10,jagupard11,jagupard12,jagupard13,jagupard14,jagupard15,jagupard16,jagupard17," \
+                           "jagupard18," \
                            "jagupard19,jagupard20,jagupard21,jagupard22,jagupard23," \
                            "jagupard24,jagupard25 \'{}\'".format(Model_FILE, COMMANDLINE)
         else:
-            full_command = "nlprun -a lisa-base-torch -m jagupard26 -p high -g 1 -n {} \'{}\'".format(Model_FILE, COMMANDLINE)
+            full_command = "nlprun -a lisa-base-torch -m jagupard26 -p high -g 1 -n {} \'{}\'".format(Model_FILE,
+                                                                                                      COMMANDLINE)
         print(full_command)
         os.system(full_command)
 
-
-
-
 #
-#nlprun -a lisa-base-torch -g 1 --p high -n 5top-finetune -x jagupard4,jagupard5,jagupard6,jagupard7,jagupard8,jagupard10,jagupard11,jagupard12,jagupard13,jagupard14,jagupard15,jagupard16,jagupard17,jagupard18,jagupard19,jagupard20,jagupard21,jagupard22,jagupard23,jagupard24,jagupard25 '$COMMANDLINE'
+# nlprun -a lisa-base-torch -g 1 --p high -n 5top-finetune -x jagupard4,jagupard5,jagupard6,jagupard7,jagupard8,
+# jagupard10,jagupard11,jagupard12,jagupard13,jagupard14,jagupard15,jagupard16,jagupard17,jagupard18,jagupard19,
+# jagupard20,jagupard21,jagupard22,jagupard23,jagupard24,jagupard25 '$COMMANDLINE'
 
-#nlprun -a lisa-base-torch -g 1 -n topk-medium-finetune -m jagupard26 '$COMMANDLINE'
-
-
-
-
-
-
-
-
+# nlprun -a lisa-base-torch -g 1 -n topk-medium-finetune -m jagupard26 '$COMMANDLINE'
 
 
 ################
@@ -409,7 +386,6 @@ if __name__ == '__main__':
 # Finetune keyword training.
 # python train_run.py keyword keywordfinetune _ 1 no no yes
 # python train_run.py embMatch embMatchfinetune _ 10 no no no
-
 
 
 ###########
@@ -452,31 +428,18 @@ if __name__ == '__main__':
 # python train_run.py data2text data2textprefixtune-no-act-cat-rep2 _ 10 no no prefixtune no (cat, epoch=5)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    #
-    #
-    # if False:
-    #     COMMANDLINE += '--dataless_sample_size {} ' \
-    #                    '--dataless_sample_length {} ' \
-    #                    '--dataless_usebaseline {} ' \
-    #                    '--dataless_control_type {} ' \
-    #                    '--gradient_accumulation_steps 20 ' \
-    #                    '--gumbel {} ' \
-    #                    '--replay_buffer {} ' \
-    #                    '--training_obj {} ' \
-    #                    '--dataless_discri_model_path {}'.format(8, 60, 'yes', 2, 'no', 'yes', 0, 'textattack/roberta-base-imdb') # 2 for sentiment;; 3 for length.
-    #                    # '--dataless_discri_model_path {}'.format(4, 60, 'yes', 2, 'no', 'yes', 0, 'textattack/roberta-base-ag-news')  # 2 for sentiment;; 3 for length.
+#
+#
+# if False:
+#     COMMANDLINE += '--dataless_sample_size {} ' \
+#                    '--dataless_sample_length {} ' \
+#                    '--dataless_usebaseline {} ' \
+#                    '--dataless_control_type {} ' \
+#                    '--gradient_accumulation_steps 20 ' \
+#                    '--gumbel {} ' \
+#                    '--replay_buffer {} ' \
+#                    '--training_obj {} ' \
+#                    '--dataless_discri_model_path {}'.format(8, 60, 'yes', 2, 'no', 'yes', 0,
+#                    'textattack/roberta-base-imdb') # 2 for sentiment;; 3 for length.
+#                    # '--dataless_discri_model_path {}'.format(4, 60, 'yes', 2, 'no', 'yes', 0,
+#                    'textattack/roberta-base-ag-news')  # 2 for sentiment;; 3 for length.
