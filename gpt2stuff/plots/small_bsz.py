ba0739d12e5968d02cd079c27f8d79133a11881c
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2021-07-05 20:05:12 -0700

    Batch size experiment.

---
+++ gpt2stuff/plots/small_bsz.py
@@ -0,0 +1,46 @@
+"""Show behavior of small batch sizes."""
+
+import os
+
+import fire
+
+from lxuechen_utils import utils
+
+
+def main(
+    base_dir="/Users/xuechenli/Desktop/dump/date_0629",
+    target_epsilon=8,
+    seed=0,
+    metric="BLEU",
+):
+    plots = []
+
+    for train_batch_size in (5, 25, 50, 100):
+        train_batch_size_str = utils.int2str(train_batch_size)
+        target_epsilon_str = utils.int2str(target_epsilon)
+
+        record_path = os.path.join(
+            base_dir,
+            f"model_name_distilgpt2_nonprivate_no_tuning_mode_prefixtune_per_example_max_grad_norm_0_10000000_noise_multiplier_-1_00000000_learning_rate_0_00500000_train_batch_size_{train_batch_size_str}_mid_dim_00000512_preseqlen_00000010_epochs_00000060_target_epsilon_{target_epsilon_str}",
+            f"{seed}",
+            "generations_score",
+            "results.json"
+        )
+        record = utils.jload(record_path)
+        x = record['global_step']
+        y = [this[metric] for this in record['score']]
+        plots.append(
+            {'x': x, 'y': y, 'label': f'batch size {train_batch_size}'}
+        )
+
+    img_path = os.path.join('.', 'gpt2stuff', 'plots', 'small_bsz', 'date_0629')
+    utils.plot(
+        img_path=img_path,
+        plots=plots,
+        options={'ylabel': metric, 'xlabel': 'iteration'}
+    )
+
+
+if __name__ == '__main__':
+    # python -m gpt2stuff.plots.small_bsz
+    fire.Fire(main)
