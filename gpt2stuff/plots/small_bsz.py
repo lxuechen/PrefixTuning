5c7bb57721d7a68e96ad77ba107cc430bbdd04ba
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2021-07-05 20:23:10 -0700

    Fix plotting.

--- gpt2stuff/plots/small_bsz.py
+++ gpt2stuff/plots/small_bsz.py
@@ -13,16 +13,18 @@ def main(
     seed=0,
     metric="BLEU",
     tuning_mode="fulltune",
+    lr=5e-4,
 ):
     plots = []
 
     for train_batch_size in (5, 25, 50, 100):
         train_batch_size_str = utils.int2str(train_batch_size)
         target_epsilon_str = utils.int2str(target_epsilon)
+        lr_str= utils.float2str(lr)
 
         record_path = os.path.join(
             base_dir,
-            f"model_name_distilgpt2_nonprivate_no_tuning_mode_{tuning_mode}_per_example_max_grad_norm_0_10000000_noise_multiplier_-1_00000000_learning_rate_0_00500000_train_batch_size_{train_batch_size_str}_mid_dim_00000512_preseqlen_00000010_epochs_00000060_target_epsilon_{target_epsilon_str}",
+            f"model_name_distilgpt2_nonprivate_no_tuning_mode_{tuning_mode}_per_example_max_grad_norm_0_10000000_noise_multiplier_-1_00000000_learning_rate_{lr_str}_train_batch_size_{train_batch_size_str}_mid_dim_00000512_preseqlen_00000010_epochs_00000060_target_epsilon_{target_epsilon_str}",
             f"{seed}",
             "generations_score",
             "results.json"
@@ -36,7 +38,7 @@ def main(
 
         argparse_path = os.path.join(
             base_dir,
-            f"model_name_distilgpt2_nonprivate_no_tuning_mode_{tuning_mode}_per_example_max_grad_norm_0_10000000_noise_multiplier_-1_00000000_learning_rate_0_00500000_train_batch_size_{train_batch_size_str}_mid_dim_00000512_preseqlen_00000010_epochs_00000060_target_epsilon_{target_epsilon_str}",
+            f"model_name_distilgpt2_nonprivate_no_tuning_mode_{tuning_mode}_per_example_max_grad_norm_0_10000000_noise_multiplier_-1_00000000_learning_rate_{lr_str}_train_batch_size_{train_batch_size_str}_mid_dim_00000512_preseqlen_00000010_epochs_00000060_target_epsilon_{target_epsilon_str}",
             f"{seed}",
             "argparse.json",
         )
@@ -45,7 +47,7 @@ def main(
         print(argparse["learning_rate"])
         print(argparse["model_name_or_path"])
 
-    img_path = os.path.join('.', 'gpt2stuff', 'plots', 'small_bsz', 'date_0629')
+    img_path = os.path.join('.', 'gpt2stuff', 'plots', 'small_bsz', f'{tuning_mode}')
     utils.plot(
         img_path=img_path,
         plots=plots,
