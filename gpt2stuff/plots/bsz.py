844d6d4c24e98bc521826285dbf6d00475d4c0f9
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2021-07-11 13:45:19 -0700

    Fix shit.

--- gpt2stuff/plots/bsz.py
+++ gpt2stuff/plots/bsz.py
@@ -13,9 +13,12 @@ def main(
 ):
     bleu_plots = []
     nll_plots = []
-    for seed in seeds:
-        for model_name_or_path in ("distilgpt2",):
-            for i, train_batch_size in enumerate((1024, 512, 256, 64, 32, 16)):
+    for model_name_or_path in ("distilgpt2",):
+        for i, train_batch_size in enumerate((1024, 512, 256, 64, 32, 16)):
+            nlls = []
+            bleus = []
+
+            for seed in seeds:
                 lr = base_lr / (2 ** i)
 
                 lr_str = utils.float2str(lr)
@@ -31,20 +34,25 @@ def main(
 
                 x = [round(hi["epoch"]) for hi in log_history]
 
-                nll = [score_i["tok_logprobs"] for score_i in log_history]
-                nll_plots.append(
-                    {'x': x, 'y': nll, 'label': f"batch size={train_batch_size}"}
-                )
+                nll = [score_i["eval"]["model"]["tok_logprobs"] for score_i in log_history]
+                nlls.append(nll)
 
                 record_path = os.path.join(train_dir, "generations_score", "results.json")
                 record = utils.jload(record_path)
 
                 score = record['score']
                 bleu = [score_i["BLEU"] for score_i in score]
+                bleus.append(bleu)
 
-                bleu_plots.append(
-                    {'x': x, 'y': bleu, 'label': f"batch size={train_batch_size}"}
-                )
+            nll_mean, nll_std = utils.average_over_seed(nlls)
+            bleu_mean, bleu_std = utils.average_over_seed(bleus)
+
+            nll_plots.append(
+                {'x': x, 'y': nll_mean, 'yerr': nll_std, 'label': f"batch size={train_batch_size}"}
+            )
+            bleu_plots.append(
+                {'x': x, 'y': bleu_mean, 'yerr': bleu_std, 'label': f"batch size={train_batch_size}"}
+            )
 
     for img_path in (
         os.path.join('.', 'gpt2stuff', 'plots', 'bsz', 'bsz_lr_joint_scaling_nll.png'),
@@ -52,7 +60,7 @@ def main(
     ):
         utils.plot(
             img_path=img_path,
-            plots=nll_plots,
+            errorbars=nll_plots,
             options={'xlabel': 'epoch', 'ylabel': 'per-token NLL'}
         )
 
@@ -62,7 +70,7 @@ def main(
     ):
         utils.plot(
             img_path=img_path,
-            plots=bleu_plots,
+            errorbars=bleu_plots,
             options={'xlabel': 'epoch', 'ylabel': 'BLEU'}
         )
 
