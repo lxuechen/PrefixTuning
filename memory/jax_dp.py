2adf7a4863ab92f082265ac2b58d627aa907ea2d
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2021-07-28 17:05:53 -0700

    Fix version.

--- memory/jax_dp.py
+++ memory/jax_dp.py
@@ -1,33 +1,108 @@
+import functools
+import itertools
+
+import fire
 import flaxmodels as fm
+from flaxmodels.gpt2 import ops
 import jax
+from jax.experimental import optimizers
 import jax.numpy as jnp
+from jax.tree_util import tree_flatten, tree_unflatten
+import numpy as np
 
-key = jax.random.PRNGKey(0)
 
-# Initialize tokenizer
-tokenizer = fm.gpt2.get_tokenizer()
+def clipped_grad(model, loss, params, l2_norm_clip, single_example_batch):
+    """Evaluate gradient for a single-example batch and clip its grad norm."""
+    grads = jax.grad(functools.partial(loss, model))(params, single_example_batch)
+    nonempty_grads, tree_def = tree_flatten(grads)
+    total_grad_norm = jnp.linalg.norm([jnp.linalg.norm(neg.ravel()) for neg in nonempty_grads])
+    divisor = jnp.maximum(total_grad_norm / l2_norm_clip, 1.)
+    normalized_nonempty_grads = [g / divisor for g in nonempty_grads]
+    return tree_unflatten(tree_def, normalized_nonempty_grads)
 
-# Encode start sequence
-generated = tokenizer.encode('The Manhattan bridge')
 
-context = jnp.array([generated])
-past = None
+def private_grad(model, loss, params, batch, rng, l2_norm_clip, noise_multiplier, batch_size):
+    """Return differentially private gradients for params, evaluated on batch."""
+    clipped_grads = jax.vmap(functools.partial(clipped_grad, model, loss), (None, None, 0))(
+        params, l2_norm_clip, batch
+    )
+    clipped_grads_flat, grads_treedef = tree_flatten(clipped_grads)
+    aggregated_clipped_grads = [g.sum(0) for g in clipped_grads_flat]
+    rngs = jax.random.split(rng, len(aggregated_clipped_grads))
+    noised_aggregated_clipped_grads = [
+        g + l2_norm_clip * noise_multiplier * jax.random.normal(r, g.shape)
+        for r, g in zip(rngs, aggregated_clipped_grads)
+    ]
+    normalized_noised_aggregated_clipped_grads = [
+        g / batch_size for g in noised_aggregated_clipped_grads
+    ]
+    return tree_unflatten(grads_treedef, normalized_noised_aggregated_clipped_grads)
 
-# Initialize model
-# Models to choose from ['gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl']
-model = fm.gpt2.GPT2LMHeadModel(pretrained='gpt2')
-params = model.init(key, input_ids=context, past_key_values=past)
 
-for i in range(20):
-    # Predict next token in sequence
-    output = model.apply(params, input_ids=context, past_key_values=past, use_cache=True)
-    token = jnp.argmax(output['logits'][..., -1, :])
-    context = jnp.expand_dims(token, axis=0)
-    # Add token to sequence
-    generated += [token]
-    # Update past keys and values
-    past = output['past_key_values']
+def make_data(seq_len=10, batch_size=16):
+    return np.random.randint(low=0, high=100, size=(batch_size, seq_len))
 
-# Decode sequence of tokens
-sequence = tokenizer.decode(generated)
-print(sequence)
+
+def next_token_loss(model, params, batch):
+    outputs = model.apply(params, input_ids=batch)
+    lm_logits = outputs["logits"]
+
+    shift_logits = lm_logits[..., :-1, :]
+    shift_labels = batch[..., 1:]
+    # flatten the tokens
+    loss = ops.cross_entropy(jnp.reshape(shift_logits, (-1, shift_logits.shape[-1])), jnp.reshape(shift_labels, (-1)))
+    return loss
+
+
+def main(
+    seq_len=100,
+    batch_size=5,
+    num_updates=100,
+    seed=42,
+
+    learning_rate=1e-4,
+    model_name_or_path="gpt2",
+    l2_norm_clip=0.1,
+    noise_multiplier=1.0,
+
+    loss=next_token_loss,
+    grad_fn=private_grad,
+
+    no_jit=False,
+):
+    rng = jax.random.PRNGKey(seed)
+    data = make_data(seq_len, batch_size)
+
+    model = fm.gpt2.GPT2LMHeadModel(pretrained=model_name_or_path)
+    params = model.init(rng, input_ids=data)
+    opt_init, opt_update, get_params = optimizers.adam(learning_rate)
+
+    def private_update(rng, i, opt_state, batch):
+        params = get_params(opt_state)
+        rng = jax.random.fold_in(rng, i)  # get new key for new random numbers
+        return opt_update(
+            i,
+            grad_fn(
+                model, loss, params, batch, rng, l2_norm_clip, noise_multiplier, batch_size
+            ),
+            opt_state
+        )
+
+    opt_state = opt_init(params)
+    itercount = itertools.count()
+    train_fn = private_update
+
+    if not no_jit:
+        train_fn = jax.jit(train_fn)
+
+    for _ in range(num_updates):
+        opt_state = train_fn(
+            rng,
+            next(itercount),
+            opt_state,
+            data,
+        )
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
