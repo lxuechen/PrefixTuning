0a40e136d7af3cd50c735ca29a790cfc879a219d
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2021-07-29 12:58:00 -0700

    Add memory scaling plots.

--- memory/plot.py
+++ memory/plot.py
@@ -14,10 +14,10 @@ sns.set_theme(style="darkgrid")
 def mode2label(mode):
     return {
         "nonprivate": "non-private",
-        "vanilla": "Opacus (chain-rule)",
-        "layer_by_layer": "layer by layer",
+        "vanilla": "chain-rule-based (Opacus)",
+        "layer_by_layer": "accumulate by layer",
         "ghost": "ghost",
-        "jax": "JAX (JIT + VMAP)"
+        "jax": "JAX (JIT + VMAP)",
     }[mode]
 
 
@@ -32,6 +32,7 @@ def main(
 ):
     os.makedirs(img_dir, exist_ok=True)
 
+    # Memory.
     grouped = collections.defaultdict(list)
     mem = utils.jload(mem_path)
     for mode in modes:
@@ -56,6 +57,42 @@ def main(
         xtick_labels = ["gpt2-small", "gpt2-medium", "gpt2-large"]
         plt.xticks(x, xtick_labels)
         plt.ylabel('max batch size')
+        plt.xlabel('model')
+        # plt.legend()  # Don't use legend for the left plot.
+        plt.tight_layout()
+        plt.savefig(img_path)
+        plt.close()
+
+    # Time.
+    grouped = collections.defaultdict(list)
+    for mode in modes:
+        for model_name_or_path in model_name_or_paths:
+            this_path = os.path.join(
+                time_dir, f'model_name_or_path_{model_name_or_path}_mode_{mode}.json'
+            )
+            if not os.path.exists(this_path):
+                grouped[mode].append(0)
+            else:
+                usage = utils.jload(this_path)
+                # Updates per min.
+                usage = usage['num_updates'] / (usage['time_elapse'] / 60)
+                grouped[mode].append(usage)
+
+    for img_path in (
+        os.path.join(img_dir, 'time.png'),
+        os.path.join(img_dir, 'time.pdf'),
+    ):
+        plt.figure(dpi=300)
+
+        for i, (mode, this_grouped) in enumerate(grouped.items()):
+            label = mode2label(mode)
+            plt.bar(
+                x + (-2 + i) * width, this_grouped, width, label=label,
+            )
+
+        xtick_labels = ["gpt2-small", "gpt2-medium", "gpt2-large"]
+        plt.xticks(x, xtick_labels)
+        plt.ylabel('gradient updates per minute')
         plt.xlabel('model')
         plt.legend()
         plt.tight_layout()
