3323b1511d29e362fde11d6f3be57ee43e087a75
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2021-08-03 15:53:45 -0700

    Activate.

--- misc_env/create_lxuechen_prefix_tuning.sh
+++ misc_env/create_lxuechen_prefix_tuning.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 conda create -n lxuechen-prefix-tuning python=3.8 -y
+sleep 3
 conda activate lxuechen-prefix-tuning
+exit
 
 pip install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
 pip install -r lxuechen_utils_requirements.txt
