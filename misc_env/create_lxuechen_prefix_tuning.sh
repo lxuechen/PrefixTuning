91b31cb848d4ef2fd3d45bc52fa5b1dd001a237f
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2021-08-03 15:35:10 -0700

    Avoid going through shit again.

---
+++ misc_env/create_lxuechen_prefix_tuning.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+conda create -n lxuechen-prefix-tuning python=3.9
+pip install -r lxuechen_utils_requirements.txt
+pip install -r ./gpt2stuff/requirements.txt
+
+# Make sure rust is installed.
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Install the modified transformers lib.
+cd ./transformers/
+pip install -e .
+cd -
