7665a4134a7b81ffff6dc27c413fa1fd53dcf090
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2021-08-03 15:41:40 -0700

    Fix shit.

--- misc_env/create_lxuechen_prefix_tuning.sh
+++ misc_env/create_lxuechen_prefix_tuning.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 conda create -n lxuechen-prefix-tuning python=3.8 -y
+
+pip install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+
 pip install -r lxuechen_utils_requirements.txt
 pip install -r ./gpt2stuff/requirements.txt
 
