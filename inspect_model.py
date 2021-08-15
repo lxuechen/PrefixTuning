1d812ac5d190edd1ac87a976b31711e7ba3da6d9
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2021-08-14 21:37:18 -0700

    Output number of parameters.

--- inspect_model.py
+++ inspect_model.py
@@ -15,18 +15,36 @@ model_cards = (
     "openai-gpt", "distilgpt2", 'gpt2', "gpt2-medium", "gpt2-large",
 )
 
+untied_params = dict()
+params = dict()
+embd_params = dict()
+
 for model_card in model_cards:
     model = transformers.AutoModelWithLMHead.from_pretrained(
-        model_card,
-        cache_dir=cache_dir
+        model_card, cache_dir=cache_dir
     )
-    num_params = sum(param.numel() for param in model.parameters()) / 1e6
-    num_embd_params = model.get_input_embeddings().weight.numel() / 1e6
+    num_params = sum(param.numel() for param in model.parameters())
+    num_embd_params = model.get_input_embeddings().weight.numel()
+    num_untied_params = num_params + num_embd_params
 
     print('---------------------')
     print(f'model: {model_card}')
     print(type(model.config))
     print(f'config: {model.config}')
-    print(f'tied model params: {num_params:.4f}')
-    print(f'untied model params: {num_params + num_embd_params:.4f}')
-    print(f'embd params: {num_embd_params:.4f}')
+
+    print(f'tied model params: {num_params / 1e6:.4f}')
+    print(f'untied model params: {num_untied_params / 1e6:.4f}')
+    print(f'embd params: {num_embd_params / 1e6:.4f}')
+
+    params[model_card] = num_params
+    untied_params[model_card] = num_untied_params
+    embd_params[model_card] = num_embd_params
+
+print('tied')
+print(repr(params))
+
+print('untied')
+print(repr(untied_params))
+
+print('embd')
+print(repr(embd_params))
